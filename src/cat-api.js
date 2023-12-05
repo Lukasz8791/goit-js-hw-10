4545f0f1786e2f3119c9b88b7fe8b93b2adc9124
Lukasz8791 <doktor8on@gmail.com>
2023-12-05 12:37:27 +0100

    result

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.headers.common[
-  'x-api-key=live_eMjAlG5v1kokIiCpQhF2XfDfOCYtaKLjkjpq3FON3ijfVHpHzpf4wbYFeJqHCJrO'
-] = 'твой ключ';
+axios.defaults.headers.common['x-api-key'] =
+  'live_eMjAlG5v1kokIiCpQhF2XfDfOCYtaKLjkjpq3FON3ijfVHpHzpf4wbYFeJqHCJrO';
 
 const fetchBreeds = async () => {
   try {
@@ -38,17 +37,25 @@ const fetchCatByBreed = async breedId => {
     document.querySelector('.error').style.display = 'none';
 
     const response = await axios.get(
-      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+      `https://api.thecatapi.com/v1/images/search?breed_ids = ${breedId}`
     );
     const catInfo = response.data[0];
+    console.log(catInfo);
 
     const catInfoDiv = document.querySelector('.cat-info');
     catInfoDiv.innerHTML = `
-      <img src="${catInfo.url}" alt="Cat Image">
-      <p><strong>Breed:</strong> ${catInfo.breeds[0].name}</p>
-      <p><strong>Description:</strong> ${catInfo.breeds[0].description}</p>
-      <p><strong>Temperament:</strong> ${catInfo.breeds[0].temperament}</p>
-    `;
+    <div class="container" style=" display: flex;
+      flex-direction: row;
+      gap: 30px;">
+    <div class="catInfoImg">
+      <img src='${catInfo.url}' alt="Cat Image" />
+    </div>
+    <div class="catInfoDesc">
+      <p><strong>Breed:</strong> ' ${catInfo.breeds[0].name}</p>
+        <p><strong>Description:</strong> ${catInfo.breeds[0].description}</p>
+        <p><strong>Temperament:</strong> ${catInfo.breeds[0].temperament}</p>
+    </div>
+    </div>`;
 
     document.querySelector('.loader').style.display = 'none';
     document.querySelector('.cat-info').style.display = 'block';
